0230b4f2b6aa1160460e23389fb54fc0cc9d6433
Nico Benninger <nico.benninger43@gmail.com>
2024-04-12 17:33:33 +0200

    fix(auth): should fix build

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.use(helmet());
+
+  app.use(cookieParser(configService.get('COOKIE_SECRET')));
+
+  app.enableCors({ credentials: true, origin: true });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,4 +23,4 @@ async function bootstrap() {
   );
   await app.listen(4000);
 }
-bootstrap();
+void bootstrap();
