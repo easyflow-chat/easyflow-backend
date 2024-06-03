0eb16c3b27e107bc2ff1f4c9318f6f6d06ded16b
Nico Benninger <nico.benninger43@gmail.com>
2024-06-03 19:16:43 +0200

    feat: inital migration

--- prisma/migrations/20240505175033_bioandname/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `name` to the `User` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `User` ADD COLUMN `bio` VARCHAR(1000) NULL,
-    ADD COLUMN `name` VARCHAR(255) NOT NULL;
--- prisma/migrations/20240603171625_initial/migration.sql
+++ prisma/migrations/20240603171625_initial/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `User`;
-
 -- CreateTable
 CREATE TABLE `user` (
     `id` VARCHAR(191) NOT NULL,
