dcbaf366204cfe8fd0c6f81fbc209e252312a035
Nico Benninger <nico.benninger43@gmail.com>
2024-11-10 21:44:41 +0100

    feat: action update

---
+++ websocket/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#Watch for errors and exit immediately
+set -e
+
+# Certificates
+echo "${CLOUDFLARE_ORIGIN_CERTIFICATE}" > /etc/ssl/backend-easyflow.pem
+echo "${CLOUDFLARE_ORIGIN_CA_KEY}" > /etc/ssl/backend-easyflow.key
+
+# Start the Go application in the background
+/app/easyflow-websocket &
+
+# Start Nginx in the foreground
+nginx -g 'daemon off;'
